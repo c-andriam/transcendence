8c309ee7705ab5c07a5bce50bc23c9f246079b38
arazafin <anjarazafinjato@gmail.com>
2025-12-19 15:20:36 +0300

    feat: Replace Fastify service with a new Node.js API Gateway setup, including dedicated Docker compose and entrypoint scripts.

--- docker/nodjs/script/entrypoint.sh
+++ docker/nodjs/script/entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Install dependencies if node_modules doesn't exist
 if [ ! -d "node_modules" ]; then
-  npm install
+  npm install --prefix /usr/src/app
 fi
 
 # Start the application
-npm run dev
+npm run dev --prefix /usr/src/app
