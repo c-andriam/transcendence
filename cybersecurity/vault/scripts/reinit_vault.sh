5bf65f4f5eda27c3bc09b32fb8616c9ba2d77166
Tantely Kanto Sambatra Ambinintsoavolamanana <tambinin@c1r13s15.42antananarivo.mg>
2025-12-22 18:19:03 +0300

    feat: Generate secure random secrets for database and API keys, and create separate Vault policies and tokens for each.

--- cybersecurity/vault/scripts/reinit_vault.sh
+++ cybersecurity/vault/scripts/reinit_vault.sh
@@ -5,6 +5,8 @@ VAULT_CONTAINER="vault" #name of the container
 VAULT_TOKEN="root"      #token admin (access to all)
 VAULT_ADDR="http://127.0.0.1:8200" #address of the vault server
 VAULT_ENV="-e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=$VAULT_ADDR" #environment variables
+SECURE_DB_SECRET=$(head -c 32 /dev/urandom | base64)
+SECURE_KEY=$(head -c 32 /dev/urandom | base64)
 
 # Waiting for vault to start before any action
 echo "Waiting for vault to start..."
@@ -25,16 +27,31 @@ docker exec $VAULT_ENV $VAULT_CONTAINER vault secrets enable -path=ft_transcende
 
 # Create the database secret
 echo "Create the database secret..."
-docker exec $VAULT_ENV $VAULT_CONTAINER vault kv put ft_transcendence/database username=db_admin password=secret_123
+docker exec $VAULT_ENV $VAULT_CONTAINER vault kv put ft_transcendence/database username=db_admin password="$SECURE_DB_SECRET"
+
+# Create the public API key secret
+echo "Create the public API key secret..."
+docker exec $VAULT_ENV $VAULT_CONTAINER vault kv put ft_transcendence/api_keys/public_api key="$SECURE_KEY"
 
 # Create the policy (only read access to the database secret)
-echo "Create the policy..."
+echo "Create the policy for database access..."
 echo 'path "ft_transcendence/data/database" { capabilities = ["read"] }' | docker exec -i $VAULT_ENV $VAULT_CONTAINER vault policy write backend-policy -
 
+# Create the policy (only read access to the API key secret)
+echo "Create the policy for API key access..."
+echo 'path "ft_transcendence/data/api_keys/*" { capabilities = ["read"] }' | docker exec -i $VAULT_ENV $VAULT_CONTAINER vault policy write api-policy -
+
 # Generate and print the token (with the policy)
-echo "Generate the token..."
-TOKEN_OUTPUT=$(docker exec $VAULT_ENV $VAULT_CONTAINER vault token create -policy=backend-policy -format=json)
-GENERATED_TOKEN=$(echo "$TOKEN_OUTPUT" | grep client_token | cut -d'"' -f4)
-echo ""
+## For db access
+echo "Generate the token for db access..."
+TOKEN_DB=$(docker exec $VAULT_ENV $VAULT_CONTAINER vault token create -policy=backend-policy -format=json | grep client_token | cut -d'"' -f4)
+
+## For api access
+TOKEN_API=$(docker exec $VAULT_ENV $VAULT_CONTAINER vault token create -policy=api-policy -format=json | grep client_token | cut -d'"' -f4)
+
+echo "-----------------------------------------------------------------------"
 echo "Token generated: "
-echo "$GENERATED_TOKEN"
+echo "For (DB) : $TOKEN_DB"
+echo ""
+echo "For (API) : $TOKEN_API"
+echo "-----------------------------------------------------------------------"
