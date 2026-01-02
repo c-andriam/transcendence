f1c527af44e01d4485d0da7fd062d1f3060e2343
Christiano Daniel Juvence Andriambeloniaina <candriam@c1r13s14.42antananarivo.mg>
2026-01-02 17:10:37 +0300

    feat: establish backend monorepo structure

--- backend/user-service/src/utils/dbPlugin.ts
+++ backend/user-service/src/utils/dbPlugin.ts
@@ -8,4 +8,8 @@ const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 const db = new PrismaClient({ adapter });
 
+console.log('[dbPlugin] Keys on db instance:', Object.keys(db));
+// @ts-ignore
+console.log('[dbPlugin] db.user exists?', !!db.user);
+
 export default db;
