b8b54e6f177ca84847726c87f88c61ab5ac11cce
Christiano Daniel Juvence Andriambeloniaina <candriam@c1r13s14.42antananarivo.mg>
2026-01-05 14:59:32 +0300

    feat: configure PrismaPg adapter with schema extracted from DATABASE_URL and update example request identifiers

--- backend/user-service/src/utils/dbPlugin.ts
+++ backend/user-service/src/utils/dbPlugin.ts
@@ -4,9 +4,15 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 import "dotenv/config";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+// const connectionString = `${process.env.DATABASE_URL}`;
+// const pool = new Pool({ connectionString });
+// const adapter = new PrismaPg(pool);
+
+const dbUrl = new URL(process.env.DATABASE_URL!);
+const schema = dbUrl.searchParams.get('schema');
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool, { schema });
+
 const db = new PrismaClient({ adapter });
 
 export default db;
