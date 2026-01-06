b7cce9b64d9a67cca1b414dd3d00e839c39fa873
Mamisedra Hariniaina Ranaivoson <mranaivo@c1r13s8.42antananarivo.mg>
2026-01-06 23:19:10 +0300

    feat & styles: Refine the creation of components such as the footer and create the first page

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import type { FooterLink } from '../pages/Footer';
+
+const Footer = () => {
+    const links: FooterLink[] = [
+        { name: 'Privacy policy', href: '/PrivacyPolicy' },
+        { name: 'Term of service', href: '/TermsOfService' },
+        { name: 'Contact us', href: '/Contact' },
+    ]
+    return (
+        <footer>
+            {/* Afficher chaque lien en utilisant map() */}
+            <nav>
+                {links.map((link) => (
+                    <a key={link.href} href={link.href}>
+                        {link.name}
+                        {link.href !== links[links.length - 1].href ? <span> | </span> : null}
+                    </a>
+                ))}
+            </nav>
+            <p>&copy; 2026 ft_transcendence. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ client/src/pages/components/HomePage.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Footer from '../../components/Footer'
+
+const HomePage = () => {
+    return (
+        <>
+            <section>
+                <div>
+                    <h1>Hello world</h1>
+                </div>
+            </section>
+            <section>
+                <div>
+                    Les 2 button Sign In et Sign Up
+                </div>
+            </section>
+            < Footer />
+        </>
+    )
+}
+
+export default HomePage
