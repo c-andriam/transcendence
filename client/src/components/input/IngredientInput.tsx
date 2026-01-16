408812e10be3860d4836b846fb26585403d47115
Mamisedra Hariniaina Ranaivoson <mranaivo@c1r13s6.42antananarivo.mg>
2026-01-16 22:30:53 +0300

    refactor: update  to manage ingredients as objects with separate name, quantity, and unit fields, improving input handling and display.

--- client/src/components/input/IngredientInput.tsx
+++ client/src/components/input/IngredientInput.tsx
@@ -1,14 +1,34 @@
 import { MdCancelPresentation } from "react-icons/md";
 import { useState } from "react";
 import { IoIosAdd } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+
+interface Ingredient {
+    name: string,
+    quantity: string,
+    unit: string
+}
 
 const IngredientInput = () => {
-    const [ingredients, setIngredients] = useState<string[]>([]);
-    const [inputValue, setInputValue] = useState<string>("");
+    // const [ingredient, setIngredient] = useState<Ingredient>({
+    //     name: "",
+    //     quantity: "",
+    //     unit: ""
+    // });
+    const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+    const [ingredientNameValue, setIngredientNameValue] = useState<string>("");
+    const [ingredientQuantityValue, setIngredientQuantityValue] = useState<string>("");
+    const [ingredientUnitValue, setIngredientUnitValue] = useState<string>("");
     const handleAddIngredient = () => {
-        if (inputValue.trim() !== "") {
-            setIngredients([...ingredients, inputValue.trim()]);
-            setInputValue("");
+        if (ingredientNameValue.trim() !== "") {
+            setIngredients([...ingredients, {
+                name: ingredientNameValue.trim(),
+                quantity: ingredientQuantityValue.trim(),
+                unit: ingredientUnitValue.trim()
+            }]);
+            setIngredientNameValue("");
+            setIngredientQuantityValue("");
+            setIngredientUnitValue("");
         }
     };
     const handleremoveIngredient = (index: number) => {
@@ -23,29 +43,35 @@ const IngredientInput = () => {
                     List of ingredient
                 </p>
             </div>
-            <div className="flex flex-wrap items-center justify-center gap-3 min-h-[42px] p-2 rounded-xl">
+            <div className="flex flex-col items-between justify-between group gap-3 min-h-[42px] rounded-xl">
                 {ingredients.length === 0 && (
                     <p className="text-gray-600 text-xs italic px-2">No ingredients added yet...</p>
                 )}
-                <ul>
+                <ul className='grid grid-cols-2 gap-x-8 gap-y-4 px-4'>
                     {ingredients.map((ingredient, index) => (
                         <li
                             key={index}
-                            className="flex items-center gap-2"
+                            className="flex items-center justify-between group py-2 border-b border-white/5 
+                       text-xs transition-all hover:border-orange-500/30"
                         >
-                            <div>
-                                <span>{ingredient}</span>
-                                <span>10</span>
-                                <span>g</span>
+                            <div className="flex items-center gap-3 truncate">
+                                <span className='w-1 h-1 rounded-full bg-orange-500/40 group-hover:bg-orange-500 transition-colors shrink-0'></span>
+                                <span className="text-white/80 truncate font-medium">{ingredient.name}</span>
                             </div>
-                            <button
-                                onClick={() => handleremoveIngredient(index)}
-                                className="cursor-pointer"
-                            >
-                                <MdCancelPresentation
-                                    size={24}
-                                />
-                            </button>
+
+                            <div className="flex items-center gap-3 shrink-0">
+                                <span className="text-white/40 font-mono">
+                                    {ingredient.quantity} {ingredient.unit}
+                                </span>
+
+                                <button
+                                    onClick={() => handleremoveIngredient(index)}
+                                    className="opacity-30 group-hover:opacity-100 cursor-pointer 
+                               text-white/20 hover:text-red-500 transition-all"
+                                >
+                                    <IoClose size={14} />
+                                </button>
+                            </div>
                         </li>
                     ))}
                 </ul>
@@ -63,14 +89,15 @@ const IngredientInput = () => {
                         name="ingredient"
                         id="ingredient"
                         placeholder="Banana"
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
+                        value={ingredientNameValue}
+                        onChange={(e) => setIngredientNameValue(e.target.value)}
                         className="bg-white/2 border-2
                             placeholder:text-gray-600 text-sm
                             p-2 rounded-lg text-white/70 w-full
                             outline-none hover:border-white/20
                             border-white/20 focus:border-orange-500/50
                             focus:ring-1 focus:ring-orange-500/20"
+                        required
                     />
                 </div>
                 <div className="flex flex-col gap-1 items-start justify-center">
@@ -85,14 +112,15 @@ const IngredientInput = () => {
                         name="quantity"
                         id="quantity"
                         placeholder="10"
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
+                        value={ingredientQuantityValue}
+                        onChange={(e) => setIngredientQuantityValue(e.target.value)}
                         className="bg-white/2 border-2 w-[10ch]
                             placeholder:text-gray-600 text-sm
                             p-2 rounded-lg text-white/70
                             outline-none hover:border-white/20
                             border-white/20 focus:border-orange-500/50
                             focus:ring-1 focus:ring-orange-500/20"
+                        required
                     />
                 </div>
                 <div className="flex flex-col gap-1 items-start justify-center">
@@ -107,8 +135,8 @@ const IngredientInput = () => {
                         name="unit"
                         id="unit"
                         placeholder="g"
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
+                        value={ingredientUnitValue}
+                        onChange={(e) => setIngredientUnitValue(e.target.value)}
                         className="bg-white/2 border-2 w-[10ch]
                             placeholder:text-gray-600 text-sm
                             p-2 rounded-lg text-white/70
@@ -125,6 +153,7 @@ const IngredientInput = () => {
                             hover:bg-orange-500/40 text-orange-500 hover:text-white/70
                             disabled:opacity-20 disabled:cursor-not-allowed
                             transition-all flex items-center justify-center"
+                        onClick={handleAddIngredient}
                     >
                         <IoIosAdd size={24} />
                     </button>
