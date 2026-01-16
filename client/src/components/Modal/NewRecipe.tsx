e00fa95aede60adc8abdada48dde549fefd4a5e1
Mamisedra Hariniaina Ranaivoson <mranaivo@c1r1s7.42antananarivo.mg>
2026-01-16 18:48:32 +0300

    feat: Implement ingredient and step input components with add/remove functionality, and refine styling for the recipe name input.

--- client/src/components/Modal/NewRecipe.tsx
+++ client/src/components/Modal/NewRecipe.tsx
@@ -2,7 +2,7 @@ import { IoMdClose } from "react-icons/io";
 import PictureInput from "../input/PictureInput";
 import RecipeNameInput from "../input/RecipeNameInput";
 import TagInput from "../input/TagInput";
-import IngredientInput from "../input/IngredientInput";
+import IngredientInput from "../input/IngredientInput"; 
 import StepInput from "../input/StepInput";
 
 const NewRecipe = ({ modalRef }: any) => {
@@ -33,32 +33,37 @@ const NewRecipe = ({ modalRef }: any) => {
                 <form action=""
                     className="flex flex-col gap-2"
                 >
-                    <RecipeNameInput />
-                    <TagInput />
-                    <IngredientInput />
-                    <StepInput />
-                    <div className="flex items-center justify-center">
+                    <div className="overflow-y-auto h-[calc(100vh-250px)] gap-2 flex flex-col px-4 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-800">
+                        <RecipeNameInput />
+                        <TagInput />
+                        <IngredientInput />
+                        <StepInput />
                         <PictureInput />
                     </div>
-                    <div className="flex flex-row justify-end gap-4">
+                    <div className="flex flex-row justify-end gap-4 px-4">
                         <button
                             type="button"
                             onClick={() => modalRef.current?.close()}
                             aria-label="Close modal"
-                            className="cursor-pointer"
+                            className="cursor-pointer rounded-lg px-7 py-2
+                            hover:bg-white/40 bg-white/10
+                            transition-all"
                         >
                             Cancel
                         </button>
                         <button
                             type="submit"
-                            className="cursor-pointer"
+                            className="cursor-pointer rounded-lg px-7 py-2
+                            bg-orange-500/20
+                            hover:bg-orange-500/40 text-orange-500 hover:text-white/70
+                            transition-allr"
                         >
                             Create
                         </button>
                     </div>
                 </form>
-            </div>
-        </dialog>
+            </div >
+        </dialog >
     );
 }
 
